3099a8e8e0a342c1b5b61b110a25f8a2fa439df5
Avinash Maske <85235293+avinash201120@users.noreply.github.com>
2023-02-07 13:02:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+(
+    function (w, d) {
+        w.CollectId = "";  //Enter your Unique ID       
+        var h = d.head || d.getElementsByTagName("head")[0];
+        var s = d.createElement("script");
+        s.setAttribute("type", "text/javascript");
+        s.async = true;
+        s.setAttribute("src", "https://collectcdn.com/launcher.js");
+        h.appendChild(s);
+    }
+    
+)
+ (window, document);
+ 
